128c36564e69d13d7a5068b1907084f967a9c98a
JereE <jere.eklund@gmail.com>
2019-04-26 21:24:22 +0300

    PositionRecorder

---
+++ Assets/_Dev/Jere/PositionPlayback.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionPlayback : MonoBehaviour
+{
+    
+    private List<Vector3> positionHistory;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_Dev/Jere/PositionRecorder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionRecorder : MonoBehaviour
+{
+    private List<Vector3> recordedPos;
+    private void FixedUpdate()
+    {
+        recordedPos.Add(transform.position);
+    }
+    
+    public List<Vector3> GetPositionHistory()
+    {
+        return recordedPos;
+    }
+
+    public void ClearRecordedPosition()
+    {
+        recordedPos.Clear();
+    }
+
+    
+}
