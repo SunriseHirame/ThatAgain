b2e47e3752fe78abac8d5dafe07e5c3d7e35e681
JereE <jere.eklund@gmail.com>
2019-04-26 22:22:00 +0300

    Ghosts spawn and reset player

--- Assets/_Dev/Jere/PositionRecorder.cs
+++ Assets/_Dev/Jere/PositionRecorder.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PositionRecorder : MonoBehaviour
 {
     private List<Vector3> recordedPos;
+    private Vector3 startPosition;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        recordedPos = new List<Vector3>();
+        startPosition = transform.position;
+    }
+
     private void FixedUpdate()
     {
         recordedPos.Add(transform.position);
     }
-    
-    public List<Vector3> GetPositionHistory()
+
+    public void ReturnToStartPosition()
     {
-        return recordedPos;
+        rb.velocity = Vector2.zero;
+        transform.position = startPosition;
+    }
+    
+    public Vector3[] GetPositionHistory()
+    {
+        return recordedPos.ToArray();
     }
 
     public void ClearRecordedPosition()
