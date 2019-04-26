4c8655f899c5a3bba03f6a91f215639d97231ab7
lqsisto <36103237+lqsisto@users.noreply.github.com>
2019-04-26 22:51:49 +0300

    Make player jump (very poorly)

--- Assets/_Game/Scripts/Controller/KinematicController.cs
+++ Assets/_Game/Scripts/Controller/KinematicController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace Game
 {
@@ -15,8 +16,20 @@ namespace Game
         
         private bool inputConsumed;
 
-        [SerializeField]
-        private Rigidbody2D attachedRigidbody;
+        private bool onGround;
+        [SerializeField] private Transform groundChecker;
+
+        //used for debugging jumping
+        [SerializeField]  private float groundDistance;
+
+        [SerializeField] private Rigidbody2D attachedRigidbody;
+
+        [SerializeField] private LayerMask groundLayerMask;
+
+        private void Start()
+        {
+            groundChecker = transform.GetChild(0);
+        }
         
         private void Update ()
         {
@@ -26,15 +39,37 @@ namespace Game
             
             inputX = Input.GetAxisRaw ("Horizontal");
             jumpInput = Input.GetAxisRaw ("Jump") > 0.1f;
+            
+            
         }
 
         private void FixedUpdate ()
-        {
+        {   
             inputConsumed = true;
             
             var dt = Time.fixedDeltaTime;         
             var xMove = inputX * speed;
-            var yMove = jumpInput ? jumpHeight : 0;
+            float yMove;
+            
+            onGround = Physics2D.OverlapCircle(
+                new Vector2(groundChecker.transform.position.x,  groundChecker.transform.position.y),
+                groundDistance,groundLayerMask 
+                );
+
+
+            if (!onGround)
+            {
+                yMove = Physics2D.gravity.y * Time.deltaTime * 5;
+            }
+            else
+            {
+                yMove = 0;
+            }
+
+            if (Input.GetButtonDown("Jump") && onGround)
+            {
+                yMove += Mathf.Sqrt(jumpHeight * -2f * Physics2D.gravity.y);
+            }
             
             var movement = new Vector2 (
                 xMove * dt, 
@@ -42,6 +77,7 @@ namespace Game
             
             attachedRigidbody.MovePosition (attachedRigidbody.position + movement);
         }
+
     }
 
 }
