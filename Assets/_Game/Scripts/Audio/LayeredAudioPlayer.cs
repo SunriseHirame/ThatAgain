c9f55b8b612b0d1c7b3fa2cc0ebe3ef7fe70dff4
Sunrise Hirame <benhard.miettinen@gmail.com>
2019-04-27 23:53:47 +0300

    LayeredAudioPlayer

---
+++ Assets/_Game/Scripts/Audio/LayeredAudioPlayer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class LayeredAudioPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioSource[] sources;
+    private int currentCombo = -1;
+
+    private void Awake ()
+    {
+        ActivateNext ();
+    }
+
+    public void Reset ()
+    {
+        currentCombo = -1;
+        foreach (var source in sources)
+        {
+            source.Stop ();
+        }
+        ActivateNext ();
+    }
+
+    public void ActivateNext ()
+    {
+        currentCombo++;
+        if (currentCombo >= sources.Length)
+            return;
+        if (!sources[currentCombo].isPlaying)
+            sources[currentCombo].Play ();
+    }
+}
