8ca6a1b54e2d7af1d1efb20dddcfe1156aed9167
lqsisto <36103237+lqsisto@users.noreply.github.com>
2019-04-27 17:32:43 +0300

    MainmenuBase

---
+++ Assets/_Game/Scripts/UI/UIController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private bool displayOnStart;
+    private Canvas currentCanvas;
+
+    private void Start()
+    {
+        currentCanvas = GetComponent<Canvas>();
+        currentCanvas.enabled = displayOnStart;
+    }
+
+    public void DisplayCanvas()
+    {
+        currentCanvas.enabled = true;
+    }
+
+    public void HideCanvas()
+    {
+        currentCanvas.enabled = false;
+    }
+    
+}
