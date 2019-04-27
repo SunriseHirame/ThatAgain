4d9b9558167ee786b46c6af0d2c4a264a8553599
Sunrise Hirame <benhard.miettinen@gmail.com>
2019-04-27 22:54:43 +0300

    Unity you are drunk

---
+++ Assets/_Game/Scripts/RagdollController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollController : MonoBehaviour
+{
+    [SerializeField] private bool initialState;
+    
+    //private HingeJoint2D[] joints;
+    private Rigidbody2D[] bodies;
+
+    private void Awake ()
+    {
+        //joints = GetComponentsInChildren<HingeJoint> ();
+        bodies = GetComponentsInChildren<Rigidbody2D> ();
+        SetRagdolling (initialState);
+    }
+
+    public void SetRagdolling (bool state)
+    {
+//        foreach (var joint in joints)
+//        {
+//            joint.enabled = state;
+//        }
+
+        foreach (var body in bodies)
+        {
+            body.simulated = state;
+        }
+    }
+}
