a7d65515a75b8ba3a09bab1ee507f66c9cf86426
lqsisto <36103237+lqsisto@users.noreply.github.com>
2019-04-27 22:45:02 +0300

    Fixed a bug where canvas would stay on when it shouldn't

--- Assets/dreamlo/HighScoreElement.cs
+++ Assets/dreamlo/HighScoreElement.cs
@@ -6,18 +6,21 @@ public class HighScoreElement : MonoBehaviour
     public TMP_Text UsernameField;
     public TMP_Text TimeField;
     public TMP_Text ScoreField;
+    public TMP_Text RankField;
 
-    public void SetValues (string username, string time, string score)
+    public void SetValues (string username, string time, string score, string rank)
     {
         UsernameField.text = username;
         TimeField.text = time;
         ScoreField.text = score;
+        RankField.text = rank;
     }
     
-    public void SetValues (string username, int time, int score)
+    public void SetValues (string username, int time, int score, int rank)
     {
         UsernameField.text = username;
         TimeField.text = (time / 1000f).ToString ("F2");
         ScoreField.text = score.ToString ();
+        RankField.text = "#" + rank.ToString();
     }
 }
