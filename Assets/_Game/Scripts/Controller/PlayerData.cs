042645402a91c44c66893cc43a45b06c697319cd
JereE <jere.eklund@gmail.com>
2019-04-27 18:10:46 +0300

    Sending score to leaderboard

---
+++ Assets/_Game/Scripts/Controller/PlayerData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Game;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    [SerializeField] private PositionRecorder positionRecorder;
+    [SerializeField] private TimeTracker timeTracker;
+    [SerializeField] private string name;
+
+    public void PushScore()
+    {
+        DreamloLeaderBoard.Instance.AddScore(name,positionRecorder.FinishCount,(int)(timeTracker.totalTime*1000));
+    }
+
+}
