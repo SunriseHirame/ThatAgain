d4d36fbf563620f447dfecedd283d4ba1d40031a
Sunrise Hirame <benhard.miettinen@gmail.com>
2019-04-27 16:29:00 +0300

    DeahLazer script

--- Assets/_Game/Scripts/DeathLazer.cs
+++ Assets/_Game/Scripts/DeathLazer.cs
@@ -3,22 +3,52 @@ using System.Collections.Generic;
 using System.Runtime.Serialization;
 using UnityEngine;
 
-public class DeathLazer : MonoBehaviour
+namespace Game
 {
-    [SerializeField] private LayerMask hitMask;
-    
-    [SerializeField] private LineRenderer lineRenderer;
-    
-    private void Update()
+    public class DeathLazer : MonoBehaviour
     {
-        
-    }
-
-    private void OnTriggerEnter2D (Collider2D other)
-    {
-        if ((1 << other.gameObject.layer) == hitMask)
+        private readonly List<Transform> targets = new List<Transform> ();
+    
+        [SerializeField] private LayerMask hitMask;    
+        [SerializeField] private LineRenderer lineRenderer;
+    
+        private void Update()
         {
-            print (other.gameObject);
+            lineRenderer.enabled = targets.Count > 0;
+            if (!lineRenderer.enabled)
+                return;
+            
+            lineRenderer.SetPosition (0, transform.position);
+            lineRenderer.SetPosition (1, targets[0].position);
+        }
+
+        private void OnTriggerEnter2D (Collider2D other)
+        {
+            if ((1 << other.gameObject.layer) != hitMask) 
+                return;
+            
+            var otherTransform = other.attachedRigidbody.transform;
+
+            if (targets.Contains (otherTransform))
+                return;
+                
+            var player = otherTransform.GetComponent<Player> ();
+            targets.Add (player ? player.Body : otherTransform);
+        }
+
+        private void OnTriggerExit2D (Collider2D other)
+        {
+            if ((1 << other.gameObject.layer) != hitMask) 
+                return;
+            
+            var otherTransform = other.attachedRigidbody.transform;
+
+            if (targets.Contains (otherTransform))
+                return;
+                
+            var player = otherTransform.GetComponent<Player> ();
+            targets.Remove (player ? player.Body : otherTransform);
         }
     }
+
 }
