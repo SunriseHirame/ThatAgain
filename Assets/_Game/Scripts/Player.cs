a640d2951cc7027a20730827910a04cff5baf1cd
Sunrise Hirame <benhard.miettinen@gmail.com>
2019-04-27 16:28:18 +0300

    DeathLazer base

---
+++ Assets/_Game/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class Player : MonoBehaviour
+    {
+        [Header ("Torso")]
+        public Transform Head;
+        public Transform Body;
+
+        [Header ("Arms")] 
+        public Transform ArmUpperLeft;
+        public Transform ArmLowerLeft;
+
+        public Transform ArmUpperRight;
+        public Transform ArmLowerRight;
+
+        [Header ("Legs")] 
+        public Transform LegUpperLeft;
+        public Transform LegLowerLeft;
+        public Transform FootLeft;
+
+        public Transform LegUpperRight;
+        public Transform LegLowerRight;
+        public Transform FootRight;
+    }
+}
