e2e8bc5edfb59576c1f9f87b3cba0cf60638c2c4
JereE <jere.eklund@gmail.com>
2019-04-27 20:25:41 +0300

    Button

---
+++ Assets/_Dev/Jere/Button.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Button : MonoBehaviour
+{
+    public UnityEvent ToggleOn;
+    public UnityEvent ToggleOff;
+
+    private bool toggledOn;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (toggledOn)
+        {
+            ToggleOff.Invoke();
+            toggledOn = false;
+        }
+        else
+        {
+            ToggleOn.Invoke();
+            toggledOn = true;
+        }
+    }
+}
