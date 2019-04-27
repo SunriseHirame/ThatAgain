df6153e4520721e6af80a57f04644ed0c6998e5f
Sunrise Hirame <benhard.miettinen@gmail.com>
2019-04-27 15:30:23 +0300

    Updated PlayerPrefab

---
+++ Assets/_Game/Scripts/DeathLazer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using UnityEngine;
+
+public class DeathLazer : MonoBehaviour
+{
+    [SerializeField] private LayerMask hitMask;
+    
+    [SerializeField] private LineRenderer lineRenderer;
+    
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        if ((1 << other.gameObject.layer) == hitMask)
+        {
+            print (other.gameObject);
+        }
+    }
+}
