7d5e1dc5cd1de9bc43ea440f0f3106580ca27953
lqsisto <36103237+lqsisto@users.noreply.github.com>
2019-04-27 18:46:37 +0300

    Functionality to display remaining health on UI added

---
+++ Assets/_Game/Scripts/UI/Ingame/HealthController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+    private List<GameObject> healthObjects = new List<GameObject>();
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            healthObjects.Add(transform.GetChild(i).gameObject);
+        }
+    }
+
+    public void LoseHealth()
+    {
+        for (var i = healthObjects.Count -1; i > -1; i--)
+        {
+            if (!healthObjects[i].GetComponent<HealthObject>().isFull)
+            {
+                continue;
+            }
+
+
+            healthObjects[i].GetComponent<HealthObject>().UiLoseHealth();
+            if (i == 0)
+            {
+                //Last Health, do something epic
+            }
+            break;
+        }
+    }
+}
