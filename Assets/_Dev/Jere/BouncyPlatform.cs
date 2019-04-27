1831c1a91b74a71c1a831fc53babe0bd06d6a77f
JereE <jere.eklund@gmail.com>
2019-04-27 15:36:52 +0300

    Bouncy platform

---
+++ Assets/_Dev/Jere/BouncyPlatform.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncyPlatform : MonoBehaviour
+{
+    [SerializeField] private LayerMask PlayerLayer;
+    [SerializeField] private float Force;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if ((1 << other.gameObject.layer) == PlayerLayer)
+        {
+            other.attachedRigidbody.AddForce(transform.up * Force,ForceMode2D.Impulse);
+        }
+    }
+}
