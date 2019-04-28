d82956249d66fd9624901e99914cfc30e7303193
JereE <jere.eklund@gmail.com>
2019-04-28 13:19:53 +0300

    Fixed stuff

--- Assets/_Game/Scripts/Environment/BouncyPlatform.cs
+++ Assets/_Game/Scripts/Environment/BouncyPlatform.cs
@@ -8,8 +8,10 @@ public class BouncyPlatform : MonoBehaviour
     [SerializeField] private float Force;
     private void OnTriggerEnter2D(Collider2D other)
     {
+        print("bounce");
         if ((1 << other.gameObject.layer) == PlayerLayer)
         {
+            print("Bounce");
             other.attachedRigidbody.velocity = Vector2.zero;
             other.attachedRigidbody.AddForce(transform.up * Force,ForceMode2D.Impulse);
         }
