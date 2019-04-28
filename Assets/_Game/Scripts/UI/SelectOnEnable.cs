8c7d5005cbe5bb0032f9a66eed8cb345725352ef
lqsisto <36103237+lqsisto@users.noreply.github.com>
2019-04-28 15:36:57 +0300

    Late fixes

---
+++ Assets/_Game/Scripts/UI/SelectOnEnable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectOnEnable : MonoBehaviour
+{
+    public Selectable Selected;
+
+    private void OnEnable()
+    {
+        Invoke(nameof(Select), 0.2f);
+    }
+
+    private void Select()
+    {
+        Selected.Select ();
+    }
+
+    private void Reset()
+    {
+        if (!Selected)
+            Selected = GetComponent<Selectable>();
+    }
+}
