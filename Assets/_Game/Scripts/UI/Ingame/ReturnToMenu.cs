f20b6a304ebcf212dd5e94058aa6a5073355681f
lqsisto <36103237+lqsisto@users.noreply.github.com>
2019-04-28 13:27:28 +0300

    Add UI to Jere's scene

---
+++ Assets/_Game/Scripts/UI/Ingame/ReturnToMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ReturnToMenu : MonoBehaviour
+{
+    void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.Escape))
+        {
+            return;
+        }
+
+        SceneManager.LoadScene(0);
+    }
+}
